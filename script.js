a3b8e94aa72547b3595dd2d8e4efc42ab48b0100
bingoasdasdawd <109863768+bingoasdasdawd@users.noreply.github.com>
2022-07-23 18:31:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 var size = 5;
-    var texts = ['akcja kompas','average promo team','dodge','ranged top','3/0 > 3/3','jg diff','top diff','kayn ban/diana gaming','5+ min queue',
+    var texts = ['akcja kompas','average promo team','dodge','ranged top','3/0 > 3/3','jg diff','top diff','banned your champ','5+ min queue',
 'afk','wtf balanced x champ rito???','lagi','65% kp','wygramy na smoki','ff15','faker mid','1v2 gank','1s ult proc',
-'ares','comeback izrael','crazy parry','8+ cs/min','penta','gg ez','0,00 died','polish mountain','toxic',
+'ares','comeback izrael','crazy reaction time','8+ cs/min','penta','gg ez','0,00 died','polish mountain','toxic',
 'win','lose','+14','+18','baron we cant end copium','dont start baron take wolves','wypierdolka z sumonerkami/ultem',
-'enchanting table language','gud game rito everything is fine ','objective steal','first pick','antyheal','duo uwu botlane','jest smok ale gank top worth bardziej'];
+'enchanting table language','gud game rito everything is fine ','objective steal','first pick','antyheal','duo uwu botlane','jest smok ale gank top worth bardziej','czytasz enemy jak ksiazke',
+            'aram on mid'];
 
     var cards = (function createCards() {
       var cards = [];
